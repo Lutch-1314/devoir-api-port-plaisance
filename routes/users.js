77ba1cee5b9e4c677ea7e48ca27ce99f583a0158
Lucie Aimar <lucie.aimar13@gmail.com>
2025-09-16 10:59:04 +0200

    private users router ok

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const service = require('../services/users');
+
+const private = require('../middlewares/private');
+
 router.get('/', service.getAllUsers);
-router.get('/:email', service.getByEmail);
+router.get('/:email', private.checkJWT, service.getByEmail);
 router.post('/', service.add);
-router.put('/:email', service.update);
-router.delete('/:email', service.delete);
+router.put('/:email', private.checkJWT, service.update);
+router.delete('/:email', private.checkJWT, service.delete);
 
 // Route pour se connecter
 router.post('/login', service.login);
